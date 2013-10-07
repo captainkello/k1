8f9ecc9547d99b40421d9cf38bd90abf9d427c76
Cap Kello <captainkello@gmail.com>
2013-10-07 12:26:46 -0700

    Add static-pages test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "/static_pages/home" do
+    it "should have the correct text" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('home')
+    end
+  end
+
+  describe "/static_pages/features" do
+    it "should have the correct text" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/features'
+      expect(page).to have_content('features')
+    end
+  end
+
+  describe "/static_pages/contact" do
+    it "should have the correct text" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      expect(page).to have_content('contact')
+    end
+  end
+end
