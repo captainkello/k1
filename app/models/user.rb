a5d562184943b7cdb6bbc1522dc9605e24162b58
Cap Kello <captainkello@gmail.com>
2013-10-08 10:21:14 -0700

    update user.rb has secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	has_secure_password
 	
 	validates :name, presence: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
