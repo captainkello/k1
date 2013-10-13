fa8db4379cbaf83db5449813f219837c2b73bc28
Cap Kello <captainkello@gmail.com>
2013-10-12 18:56:28 -0700

    add @current_user to SessionHelper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			sign_in user # Sign the user in does not exist
+			sign_in user # sign_in method is in SessionHelper.rb
 			redirect_to user
 		else
 			# Create an error message and re-render the signin form.
