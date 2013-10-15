bb2c06375ab434beec70e9a932ad32e009a398b6
Cap Kello <captainkello@gmail.com>
2013-10-14 19:24:55 -0700

    Add admin user delete before_action

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,8 +12,18 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:admin)}
   # it { should respond_to(:authenticate)}  why is this in 8.15?
   it { should respond_to(:remember_token)}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "remember token" do
     before { @user.save }
